ed93e06f241533613526850fd636b39c821f484b
LucaOrlandi98 <orlandiluca00@gmail.com>
2026-03-26 20:02:14 +0100

    Fix GitHub Pages asset base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(function (_a) {
     var mode = _a.mode;
     return ({
-        base: mode === "production" ? "/PANDA-PROJECT/" : "/",
+        base: mode === "production" ? "./" : "/",
         plugins: [react()],
     });
 });
